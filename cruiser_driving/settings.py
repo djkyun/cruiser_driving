4274bad23abf01bad54482371c95bde8dc18068d
djkyun <haptic.shenzhen@gmail.com>
2021-10-24 03:39:59 +0800

    fifth commit

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -36,7 +36,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1','cruiser_driving.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1','cruiserdriving.herokuapp.com']
 
 
 
