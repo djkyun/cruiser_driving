78aa6485e7eacab06fdddfdffe75f2985675d549
djkyun <haptic.shenzhen@gmail.com>
2021-10-26 03:10:21 +0800

    change settings add base dir

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -146,6 +146,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
