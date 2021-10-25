910991b4ad1a3361f0f6fcbe7a47b8752cd4009f
djkyun <haptic.shenzhen@gmail.com>
2021-10-26 02:55:48 +0800

    change database settings

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -98,12 +98,8 @@ WSGI_APPLICATION = 'cruiser_driving.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cruiser_driving',
-        'USER': 'postgres',
-        'PASSWORD': '9944',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
