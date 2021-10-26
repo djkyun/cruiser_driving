4da2271225b122cc1e75bf83b80ba8423f5419dd
djkyun <haptic.shenzhen@gmail.com>
2021-10-26 21:34:40 +0800

    maraming changes basta

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'mksm&9x)#xt(#eftehe3#qcrr0@js5d!lq#cjket7!8=0j3m#j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1','cruiserdriving.herokuapp.com']
 
 
 # Application definition
