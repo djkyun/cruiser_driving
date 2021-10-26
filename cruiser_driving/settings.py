dc83c144c7e48e3ea537a52df7d42c197656cf0d
djkyun <haptic.shenzhen@gmail.com>
2021-10-26 21:24:54 +0800

    removed STATICFILES_DIRS

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -154,9 +154,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
