40710a9654b0c0433d61131039f8c1a3065b4024
djkyun <haptic.shenzhen@gmail.com>
2021-11-01 00:26:56 +0800

    change to postgresql config from local postgresql

--- cruiser_driving/settings.py
+++ cruiser_driving/settings.py
@@ -120,10 +120,10 @@ WSGI_APPLICATION = 'cruiser_driving.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cruiser_driving',
-        'USER': 'postgres',
-        'PASSWORD': '9944',
-        'HOST':'127.0.0.1',
+        'NAME': 'dah0betphdn1lj',
+        'USER': 'vrzgpoxxhjhfcj',
+        'PASSWORD': 'c99f860b5e7f98ad22a2e3fba83046a5307899be550ba5eb3bc704f28f60f72b',
+        'HOST':'ec2-34-239-55-93.compute-1.amazonaws.com',
         'PORT':'5432',
     }
 }
