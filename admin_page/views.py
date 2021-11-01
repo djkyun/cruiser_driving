9805fab1a1e7c9564ce4d6bc3782f54c40c32319
djkyun <haptic.shenzhen@gmail.com>
2021-11-02 06:58:49 +0800

    removed Count import in admin_page views

--- admin_page/views.py
+++ admin_page/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import LoginHistory, Course, SystemSettings, Category, PaymentMethodList, Enrolment, SalesTransaction, Appointment, TimeScheduleAppointment, ScheduleType, Attendance, Rooms, Lesson, LessonDetailTitle, LessonDetail, InstructorSpecialization, CategoryType, CarType
 from website.models import UserRecords
-from django.db.models import Q, Sum, Avg, Count
+from django.db.models import Q, Sum, Avg
 from django.contrib.auth.models import User
 from django.contrib.auth import login as login_form, authenticate, logout as logout_form
 from django.contrib import messages
